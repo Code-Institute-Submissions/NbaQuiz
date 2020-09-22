49944a8a64a42aef48c414fa0ee41a01224b24c3
albert5480jnr <aj_sam@hotmail.co.uk>
2020-09-22 20:26:20 +0000

    points and max questions error fixed

--- Assets/js/quiz.js
+++ Assets/js/quiz.js
@@ -497,7 +497,7 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('Last.html')
